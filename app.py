6954a9ac51cc71d40362e40dad3283db84627939
yefeyfe <pintu.se.ju@gmail.com>
2023-08-27 09:21:12 +0530

    feat(task_queue): Added support for redis task queue

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import redis
+from rq import Queue
+
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
@@ -18,6 +21,8 @@ def create_app(db_url=None):
     app = Flask(__name__)
     load_dotenv()
 
+    connection = redis.from_url(os.getenv("REDIS_URL"))
+    app.queue = Queue("emails", connection=connection)
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
